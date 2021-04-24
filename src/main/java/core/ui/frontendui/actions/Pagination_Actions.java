0212f3b2c1a430db3f4d6cdbf9eed9336ed15ea7
subhadip sinha <subhadip.sinha@unbxd.com>
2021-04-24 18:08:57 +0530

    SDKV2

---
+++ src/main/java/core/ui/frontendui/actions/Pagination_Actions.java
@@ -0,0 +1,104 @@
+package core.ui.frontendui.actions;
+
+import core.ui.frontendui.page.Pagination_Page;
+import javafx.scene.control.Pagination;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
+
+public class Pagination_Actions extends Pagination_Page {
+    SoftAssert softAssert = new SoftAssert();
+    //public Select sortList= new Select(getDriver().findElement(By.xpath(sortByDropdown)));
+    //Select sortList;
+
+
+    public Pagination_Actions goToWebsite(String url) {
+        getDriver().navigate().to(url);
+        awaitForPageToLoad();
+        return this;
+    }
+    public void searchQuery(String query) throws InterruptedException {
+        await();
+        awaitForPageToLoad();
+        awaitForElementPresence(searchBox);
+        click(searchBox);
+        searchBox.clear();
+        searchBox.fill().with(query);
+        searchBox.submit();
+        await();
+    }
+
+    public void testPaginationIsDisplayingOrNot()
+    {
+        await();
+        softAssert.assertEquals(unbxdPagination.isDisplayed(),true,"Fail: Pagination is not present");
+        System.out.println("Pass: pagination is getting displayed");
+    }
+    public void testPaginationIsWorkingOrNot()
+    {
+        await();
+        int cPage=Integer.parseInt(currentPage.getText());
+        goTo2ndPage.click();
+        awaitForPageToLoad();
+        softAssert.assertEquals(Integer.parseInt(currentPage.getText()),cPage+1,"Fail: Pagination is not working");
+        System.out.println("Pass: Pagination is working");
+    }
+
+    public void testPaginationBackArrowIsWorking()
+    {
+        await();
+        int oldPageNo=Integer.parseInt(currentPage.getText());
+        click(backwardArrow);
+        awaitForPageToLoad();
+        softAssert.assertEquals(Integer.parseInt(currentPage.getText()),oldPageNo-1,"Pagination back arrow key is not working.");
+        System.out.println("Pass: Pagination Back arrow key is working.");
+    }
+    public  void testPaginationForwardArrowIsWorking(){
+        await();
+        int oldPageNo=Integer.parseInt(currentPage.getText());
+        forwardArrow.click();
+        awaitForPageToLoad();
+        softAssert.assertEquals(Integer.parseInt(currentPage.getText()),oldPageNo+1,"Forward Arrow key is working");
+        System.out.println("Pass: Pagination forward arrow key is working.");
+    }
+
+    public void testSelectedPageHighlightOrNot()
+    {
+        int oldPageNo=Integer.parseInt(currentPage.getText());
+        click(forwardArrow);
+        await();
+        int newPageNo=Integer.parseInt(currentPage.getText());
+        softAssert.assertEquals(newPageNo,oldPageNo+1,"Fail: Selected page is not getting highlighted");
+        System.out.println("Pass: Selected page is highlighted.");
+
+    }
+
+//    public void  testPaginationScenarioInCaseOfLastPageResults() throws InterruptedException {
+//        //String max="";
+//        searchQuery(productTitle);
+//        Thread.sleep(2000);
+//        String result=productDescription.getText();
+//        String[] totalSrcResult=result.split("of");
+//        int totalResult=Integer.parseInt(totalSrcResult[1].replace("products","").replace(" ",""));
+//        Select proPerPage=new Select(getDriver().findElement(By.xpath(productPerPageDropDown)));
+//        //proPerPage.selectByValue("20");
+//        List<WebElement> allOption=proPerPage.getOptions();
+//        proPerPage.selectByVisibleText(allOption.get(allOption.size()-1).getText());
+//        int maxNum=Integer.parseInt(allOption.get(allOption.size()-1).getText());
+//        int i=0;
+//        while(i<totalResult/maxNum) {
+//            clickPage();
+//            i++;
+//        }
+//       List<WebElement> pageArrows=getDriver().findElements(By.xpath(paginationArrowIcon));
+//       for(int j=0;j<pageArrows.size();j++) {
+//           if (pageArrows.get(j).getText() != ">")
+//               System.out.println("Pass");
+//           else if (pageArrows.get(j).getText() == ">")
+//               System.out.println("Fail");
+//       }
+//    }
+}
---
+++ src/main/java/core/ui/frontendui/page/Pagination_Page.java
@@ -0,0 +1,33 @@
+package core.ui.frontendui.page;
+
+import core.ui.page.UiBase;
+import org.fluentlenium.core.domain.FluentWebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Pagination_Page extends UiBase {
+    @FindBy(xpath = "//input[@class='UNX-searchbox__input']")
+    public FluentWebElement searchBox;
+    @FindBy(xpath = "//button[@data-testid='UNX_pageNumber2']")
+    public FluentWebElement goTo2ndPage;
+    @FindBy(xpath = "//button[text()='>']")
+    public static FluentWebElement forwardArrow;
+    @FindBy(xpath = "//button[text()='<']")
+    public FluentWebElement backwardArrow;
+    @FindBy(xpath = "//button[@class='UNX-pageNavigation__button -selected']")
+    public static FluentWebElement currentPage;
+    @FindBy (xpath = "//div[@class='UNX-pageNavigation__container']")
+    public FluentWebElement unbxdPagination;
+    public String productTitle="Beverly Hills Polo Club";
+    public String productPerPageDropDown="//select[@class='UNX-pageSize__dropdown']";
+    @FindBy(xpath = "//span[@class='-pageDescription']")
+    public FluentWebElement productDescription;
+//    @FindBy(xpath = "//button[text()='>']")
+//    public static FluentWebElement forwardArrow;
+    public String paginationArrowIcon="//button[@class='UNX-pageNavigation__button -action']";
+    public void clickPage() throws InterruptedException {
+        forwardArrow.click();
+        Thread.sleep(2000);
+    }
+
+
+}
